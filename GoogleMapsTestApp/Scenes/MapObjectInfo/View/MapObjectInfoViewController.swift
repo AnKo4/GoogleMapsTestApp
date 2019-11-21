c8854fb3a8839e95cc2667725a5d4897cf2d6af9
Andrey Kochetkov <akochetkov@65apps.com>
2019-11-21 13:08:11 +0400

    Start creating MapObjectInfo scene

---
+++ GoogleMapsTestApp/Scenes/MapObjectInfo/View/MapObjectInfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapObjectInfoViewController.swift
+//  GoogleMapsTestApp
+//
+//  Created by Andrey on 21.11.2019.
+//  Copyright © 2019 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class MapObjectInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
